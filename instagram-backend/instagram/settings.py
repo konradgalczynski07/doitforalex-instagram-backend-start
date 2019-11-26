23cc92548ca3f92355fb54da1de771c63f1b585f
Konrad Galczynski <konradgalczynski07@gmail.com>
2019-11-26 14:45:19 +0100

    add clena user app

--- instagram-backend/instagram/settings.py
+++ instagram-backend/instagram/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -102,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
