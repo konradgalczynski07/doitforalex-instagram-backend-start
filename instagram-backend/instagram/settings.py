1e6536d846a8d878080a10a5c32410bd0edd864b
Konrad Galczynski <konradgalczynski07@gmail.com>
2019-11-26 17:18:03 +0100

    add drf settings

--- instagram-backend/instagram/settings.py
+++ instagram-backend/instagram/settings.py
@@ -131,3 +131,17 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ADMIN_URL = os.environ.get('ADMIN_URL', 'admin/')
+
+
+# Django Rest Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 9,
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
