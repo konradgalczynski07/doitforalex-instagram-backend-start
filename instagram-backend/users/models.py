ffd3044656bd7f0e38e33236968b404d0964c5cb
Konrad Galczynski <konradgalczynski07@gmail.com>
2019-12-04 18:39:46 +0100

    add user profile and followers

--- instagram-backend/users/models.py
+++ instagram-backend/users/models.py
@@ -8,6 +8,24 @@ class User(AbstractUser):
     fullname = models.CharField(max_length=60, blank=True)
     bio = models.TextField(blank=True)
     profile_pic = models.ImageField(default='avatar.png')
+    followers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="user_followers",
+        blank=True,
+        symmetrical=False,
+    )
+    following = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="user_following",
+        blank=True,
+        symmetrical=False,
+    )
+
+    def number_of_followers(self):
+        return self.followers.count()
+
+    def number_of_following(self):
+        return self.following.count()
 
     def __str__(self):
         return self.username
--- instagram-backend/users/views.py
+++ instagram-backend/users/views.py
@@ -1,5 +1,16 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
-from users.serializers import RegisterUserSerializer, UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from users.serializers import (
+    UserSerializer,
+    RegisterUserSerializer,
+    UserProfileSerializer,
+)
+
+
+User = get_user_model()
 
 
 class RegisterUserView(generics.CreateAPIView):
@@ -12,3 +23,28 @@ class UserView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class UserProfileView(generics.RetrieveAPIView):
+    lookup_field = 'username'
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class FollowUserView(APIView):
+    def get(self, request, format=None, username=None):
+        to_user = User.objects.get(username=username)
+        from_user = self.request.user
+        follow = None
+        if from_user != to_user:
+            if from_user in to_user.followers.all():
+                follow = False
+                from_user.following.remove(to_user)
+                to_user.followers.remove(from_user)
+            else:
+                follow = True
+                from_user.following.add(to_user)
+                to_user.followers.add(from_user)
+        data = {'follow': follow}
+        return Response(data)
